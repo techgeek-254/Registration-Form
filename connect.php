5342fe1e9b7695e0b77555cbf00ef7ecf6668ecf
John Mutua <jmutua@integrity.go.ke>
2024-04-15 02:10:05 +0300

    First Release of Registration Page

---
+++ connect.php
@@ -0,0 +1,40 @@
+<?php
+
+	$firstName = $_POST['firstName'];
+	$lastName = $_POST['lastName'];
+	$email = $_POST['email'];
+	$mediaHouse = $_POST['mediaHouse'];
+	$mobileNumber = $_POST['mobileNumber'];
+	$mckNumber = $_POST['mckNumber'];
+	$designation = $_POST['designation'];
+
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbName = "test";
+
+
+	//create connection
+	$conn = new mysqli($servername, $username, $password,'test');
+
+	// Database connection
+	
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : " . $conn->connect_error);
+	} 
+	else 
+	{
+
+		$stmt = $conn->prepare("insert into registration(firstName, lastName, email, mediaHouse, mobileNumber, mckNumber, designation) values(?, ?, ?, ?, ?, ?, ?)");
+		$stmt->bind_param("ssssiis", $firstName, $lastName, $email, $mediaHouse, $mobileNumber, $mckNumber, $designation);
+		$execval = $stmt->execute();
+		//echo $execval;
+		
+		//echo "Registration successfull...";
+		echo '<script>alert("Registration Successful...");
+		window.location="http://localhost:80/test"</script>'; 
+		$stmt->close();
+		$conn->close();
+
+	}
---
+++ index.js
@@ -0,0 +1,10 @@
+//Display current time with seconds
+setInterval(myFunction, 1000);
+function myFunction() {
+  let d = new Date();
+  document.getElementById("demo").innerHTML=
+  d.getHours() + ":" +
+  d.getMinutes() + ":" +
+  d.getSeconds();
+}
+
