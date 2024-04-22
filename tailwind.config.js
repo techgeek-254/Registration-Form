29f9a9098d163759ef0a83f4e27f8b41ccac0709
John Mutua <johnstone.m10@gmail.com>
2024-04-22 10:47:35 +0300

    slight changes in tailwindconfig file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./test/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
